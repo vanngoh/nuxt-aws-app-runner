7ca5438829c3189275a700d6801b14016a488c20
Vann <vanngoh.dev@gmail.com>
2024-12-21 15:33:02 +0900

    add the page index for demo

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4 space-x-2">
+    <div>Hello world</div>
+    <UButton @click="getUser('1234')">Get All User</UButton>
+    <div>{{ users }}</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const users = ref<any>({});
+  const getUser = async (uid: string) => {
+    const data = await $fetch(`/api/users/${uid}`);
+    console.log(data);
+    users.value = data;
+  };
+</script>
